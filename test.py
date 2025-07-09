9a3ff0a8b930b47456ccb4f4a5888b0a28f62440
Hamid Qammar <hamidproacc@gmail.com>
2025-07-09 12:35:59 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 print("My name is Hamid Qammar.")
 print("I am a DevOps Engineer.")
 print("I have also interest in Cybersecurity.")
-print("I am also a Cybersecurity Analyst.")
+print("I am also a Cybersecurity Analyst."
